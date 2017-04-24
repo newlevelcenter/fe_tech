6b63ed95bc09af76b0dfc8e8af4d7e8fcf7fffed
newlevelcenter <newlevelcenter@gmail.com>
2017-04-24 22:20:52 +0300

    new

---
+++ 30_jsonp/jsonp2/user - копия.php
@@ -0,0 +1,35 @@
+<?php
+
+// Устанавливаем возможность отправлять ответ для любого домена
+header('Access-Control-Allow-Origin: *');
+
+if (version_compare(phpversion(), '5.3.0', '>=')  == 1)
+  error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
+else
+  error_reporting(E_ALL & ~E_NOTICE); 
+
+// Получаем параметры POST
+$id = $_REQUEST['id'];
+$callback = $_REQUEST['callback'];
+// Выполняем вычисления
+class Coor {
+// данные (свойства):
+var $name;
+
+var $id;
+
+}
+$object = new Coor;
+
+$object->name = "Alex";
+$object->id = $id;
+
+      $aResult = array(
+    'result' => 'Input: p1, p2. And sign -  a. a=1 - plus, a=2 - minus, a=3 - multiplication, a=4 - devide');  
+
+
+
+
+// Генерируем результат
+header('Content-type: application/json');
+echo json_encode($object);
